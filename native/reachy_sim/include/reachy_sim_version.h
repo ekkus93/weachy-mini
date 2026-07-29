d734ac83ce53321dd5c0a26714dc77e7c9fddde2
Phillip Chin <ekkus93@gmail.com>
2026-07-29 02:21:09 -0700

    RMA-030: map scaffold version API to stable ABI

--- native/reachy_sim/include/reachy_sim_version.h
+++ native/reachy_sim/include/reachy_sim_version.h
@@ -1,13 +1,15 @@
 #ifndef REACHY_SIM_VERSION_H
 #define REACHY_SIM_VERSION_H
 
-#include <stdint.h>
+#include "reachy_sim.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-enum { REACHY_SIM_SCAFFOLD_ABI_VERSION = 1 };
+enum {
+    REACHY_SIM_SCAFFOLD_ABI_VERSION = REACHY_SIM_ABI_VERSION
+};
 
 uint32_t reachy_sim_scaffold_abi_version(void);
 const char* reachy_sim_scaffold_version_string(void);
